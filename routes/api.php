c478da62e1a69e572a94551b0ecd8a7251adf372
Austin Velásquez <austinvg.dev@gmail.com>
2022-10-10 00:11:33 -0600

    [ADD] first routes for API

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\AchievementController;
+use App\Http\Controllers\ActivityDifficultyController;
+use App\Http\Controllers\ActivityTypeController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ThemeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +19,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+$versionApi = config('app.api_version');
+
+
+
+Route::group([
+    'prefix'     => "/$versionApi",
+], function () {
+
+    Route::controller(AuthController::class)->prefix('/auth')->group(function () {
+        Route::post('/login', 'loginUser')->name('login');;
+        Route::post('/register', 'createUser')->name('register');
+    });
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::controller(AchievementController::class)->prefix('/achievements')->group(function () {
+            Route::get('/', 'index');
+            Route::get('/{id}', 'show');
+        });
+
+        Route::controller(ActivityDifficultyController::class)->prefix('/activity-difficulties')->group(function () {
+            Route::get('/', 'index');
+            Route::get('/{id}', 'show');
+        });
+
+        Route::controller(ActivityTypeController::class)->prefix('/activity-types')->group(function () {
+            Route::get('/', 'index');
+            Route::get('/{id}', 'show');
+        });
+
+        Route::controller(ThemeController::class)->prefix('/themes')->group(function () {
+            Route::get('/', 'index');
+            Route::get('/{id}', 'show');
+        });
+    });
 });
